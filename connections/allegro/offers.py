c9d80f6d850c37ec7155389c7776416abd9c6bfb
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-07-08 10:14:23 +0200

    Expanded Allegro Offers

--- connections/allegro/offers.py
+++ connections/allegro/offers.py
@@ -58,25 +58,47 @@ class AllegroOffers:
         return response.json()
 
     def get_all_offers(self):
-        url = f'{config.ALLEGRO_API_URL}/sale/offers'
+        """Get all offers from Allegro API.
+        Returns:
+            list: A list of all offers if successful, or an Error Dict if failed.
 
+        https://developer.allegro.pl/documentation#tag/User's-offer-information/operation/searchOffersUsingGET
+        """
+        url = f'{config.ALLEGRO_API_URL}/sale/offers'
         all_offers = []
 
-        product_limit = 100
-        offset = 0
+        params = {'limit': 100, 'offset': 0}
 
-        params = {'limit': product_limit, 'offset': offset}
         response = self.client.session.request('GET', url, params=params)
+        response_json = response.json()
+
+        if response.status_code != 200:
+            if 'errors' in response_json and isinstance(response_json['errors'], list):
+                error_message = response_json['errors'][0].get('message', 'Unknown error')
+            else:
+                error_message = response_json.get('error_description', 'Unknown error')
+
+            return {'success': False, 'error': error_message}
+        
         data = response.json()
         total_products = data.get('totalCount', 0)
 
-        print(f"Total offers available: {total_products}")
-
-        for offset in tqdm(range(0, total_products, product_limit), desc="Downloading offers", unit=" product"):
-            params = {'limit': product_limit, 'offset': offset}
+        for offset in tqdm(range(0, total_products, 100),
+                        desc="Downloading offers", unit=" product"):
+            
+            params['offset'] = offset
             response = self.client.session.request('GET', url, params=params)
-            data = response.json()
-            offers = data.get('offers', [])
+            response_json = response.json()
+
+            if response.status_code != 200:
+                if 'errors' in response_json and isinstance(response_json['errors'], list):
+                    error_message = response_json['errors'][0].get('message', 'Unknown error')
+                else:
+                    error_message = response_json.get('error_description', 'Unknown error')
+
+                return {'success': False, 'error': error_message}
+            
+            offers = response_json.get('offers', [])
 
             if not offers:
                 break
